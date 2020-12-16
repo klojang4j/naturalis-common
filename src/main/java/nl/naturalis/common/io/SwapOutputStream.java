cf852923eaa78ec73753a932a8c8030de39030c2
Ayco Holleman <ayco.holleman@naturalis.nl>
2020-12-16 17:57:18 +0100

    comments

--- src/main/java/nl/naturalis/common/io/SwapOutputStream.java
+++ src/main/java/nl/naturalis/common/io/SwapOutputStream.java
@@ -4,20 +4,25 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.channels.FileChannel;
 import nl.naturalis.common.check.Check;
 
 /**
- * An extension of {@code OutputStream} that allows for data written to it to be recalled. Data
- * written to a {@code SwapOutputStream} first fills up an internal buffer. If (and only if) the
- * buffer reaches full capacity, a swap file is created to sink the data into. This ensures that
- * whatever the total size of the data written to a {@code SwapOutputStream}, it can always be
+ * An {@code OutputStream} that allows allows you to read back the data that were written to it.
+ * Data written to a {@code SwapOutputStream} first fills up an internal buffer. If (and only if)
+ * the buffer reaches full capacity, a swap file is created to sink the data into. This ensures that
+ * whatever the total size of the data written to the {@code SwapOutputStream}, it can always be
  * recalled. It is transparent to clients whether or not data has actually been swapped out of
  * memory. Clients can recall the data without having to know whether it came from the internal
  * buffer or from the swap file.
  *
- * <p>In many respects {@code SwapOutputStream} and its subclasses work just like a {@link
- * BufferedOutputStream}. Therefore it doesn't make much sense to wrap a {@code SwapOutputStream}
- * into a {@code BufferedOutputStream}.
+ * <p>After you recalled the data you can continue writing data to the {@code SwapOutputStream}. It
+ * has in effect become a {@link BufferedOutputStream} around the output stream that you passed to
+ * the {@link #recall(OutputStream) recall} method. For this and other reasons it doesn't make sense
+ * to wrap a {@code SwapOutputStream} into a {@code BufferedOutputStream}.
+ *
+ * <p>{@code SwapOutputStream} and its subclasses are not thread-safe. Except for the {@link
+ * #cleanup()} method als method calls need to be synchronized using a lock on the entire instance
  *
  * @author Ayco Holleman
  */
@@ -52,9 +57,9 @@ public abstract class SwapOutputStream extends OutputStream {
   public abstract void recall(OutputStream target) throws IOException;
 
   /**
-   * Deletes the swap file. Can be called if, after the data has been recalled, the swap file is no
-   * longer needed. You might also want to call this method in the {@code catch} block of an
-   * exception.
+   * Deletes the swap file, if it was created. Can be called if, after the data has been recalled,
+   * the swap file is no longer needed. You might also want to call this method in the {@code catch}
+   * block of an exception.
    */
   public void cleanup() {
     if (swapFile.exists()) {
@@ -63,8 +68,17 @@ public abstract class SwapOutputStream extends OutputStream {
   }
 
   /**
+   * Closes the {@link OutputStream} or {@link FileChannel} (depending on the implementation)
+   * writing to the swap file. It will not close the output stream specified by the {@link
+   * #recall(OutputStream)} method. The {@code SwapOutputStream} will "swallow" this output stream
+   * to turn itself into a {@code BufferedOutputStream} around the output stream. However you don't
+   * have it make use of this, so closing the output stream is left to the user.
+   */
+  public abstract void close() throws IOException;
+
+  /**
    * Returns whether or not a swap file was created. You should not normally need to call this
-   * method as swapping is taken care of automatically, but it could be used for debug or logging
+   * method as swapping is taken care of automatically, but it could be useful for debug or logging
    * purposes.
    */
   public abstract boolean hasSwapped();
