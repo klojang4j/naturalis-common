63836446822d1f788e3415d1c9ab63fdebe3c82e
ayco <ayco@ayco-Latitude-E5470>
2021-01-11 15:51:17 +0100

    removed import

--- src/test/java/nl/naturalis/common/collection/IntListTest.java
+++ src/test/java/nl/naturalis/common/collection/IntListTest.java
@@ -1,7 +1,6 @@
 package nl.naturalis.common.collection;
 
 import org.junit.Test;
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
