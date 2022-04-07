945123c82ae26e04ad663a0afb9e1391c51d45de
Ayco Holleman <ayco.holleman@naturalis.nl>
2022-04-07 17:10:46 +0200

    PrimitiveArraySegmentReader no longer uses Path.isArrayIndex

--- src/main/java/nl/naturalis/common/path/PrimitiveArraySegmentReader.java
+++ src/main/java/nl/naturalis/common/path/PrimitiveArraySegmentReader.java
@@ -1,10 +1,13 @@
 package nl.naturalis.common.path;
 
-import static nl.naturalis.common.path.PathWalkerException.pathExtendsBeyondPrimitive;
+import static nl.naturalis.common.ObjectMethods.isEmpty;
+import static nl.naturalis.common.path.PathWalkerException.*;
 
 import java.lang.reflect.Array;
+import java.util.OptionalInt;
 import java.util.function.Function;
 
+import nl.naturalis.common.NumberMethods;
 import nl.naturalis.common.path.PathWalker.OnDeadEnd;
 
 final class PrimitiveArraySegmentReader extends SegmentReader<Object> {
@@ -17,13 +20,19 @@ final class PrimitiveArraySegmentReader extends SegmentReader<Object> {
   Object read(Object array, Path path) {
     if (path.size() == 1) { // primitive *must* be the end of the trail
       String segment = path.segment(0);
-      if (Path.isArrayIndex(segment)) {
-        int idx = Integer.parseInt(segment);
-        if (idx < Array.getLength(array)) {
-          Object val = Array.get(array, idx);
-          return nextSegmentReader().read(val, path.shift());
-        }
+      if (isEmpty(segment)) {
+        return deadEnd(() -> emptySegment(path));
       }
+      OptionalInt opt = NumberMethods.toPlainInt(segment);
+      if (opt.isEmpty()) {
+        return deadEnd(() -> arrayIndexExpected(path));
+      }
+      int idx = opt.getAsInt();
+      if (idx < Array.getLength(array)) {
+        Object val = Array.get(array, idx);
+        return nextSegmentReader().read(val, path.shift());
+      }
+      return deadEnd(() -> indexOutOfBounds(path));
     }
     return deadEnd(() -> pathExtendsBeyondPrimitive(path));
   }
